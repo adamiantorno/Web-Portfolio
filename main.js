1d55ee789c8acc0013318f66077fe16f80a533a3
Adam I <adam.iantorno99@gmail.com>
2022-06-09 23:47:20 -0400

    Create-React-App

--- main.js
+++
@@ -1,9 +0,0 @@
-
-function display_menu() {
-    var menu = document.getElementById('sidebar-nav-list');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
