87d7edd7a36da1e34de2395b5accec3d06041481
Adam I <adam.iantorno99@gmail.com>
2022-06-26 01:28:44 -0400

    exp func

--- web-portfolio/src/assets/expdata.js
+++ web-portfolio/src/assets/expdata.js
@@ -1,46 +1,55 @@
 const ExpData = [
     {
-        id: "1",
-        company: "416 Automation",
-        title: "Mechatronics Engineer",
-        period: "Jan 2020 - April 2020",
-        img: "./imgs/",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    },
-    {
-        id: "2",
-        company: "Flipp",
-        title: "Quality Assurance Intern",
-        period: "Jan 2020 - April 2020",
-        img: "./imgs/",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    },
-    {
-        id: "3",
-        company: "GrantMatch",
-        title: "Junior Software Engineer",
-        period: "Jan 2020 - April 2020",
-        img: "./imgs/",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    },
-    {
-        id: "4",
-        company: "UWAFT",
-        title: "Sensor Diagnostic Subteam Lead",
-        period: "Jan 2020 - April 2020",
-        img: "./imgs/",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    },
-    {
-        id: "4",
+        id: 0,
         company: "Electrans",
         title: "Mechatronics Engineer",
         period: "Jan 2020 - April 2020",
         img: "./imgs/",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
     },
-
-
+    {
+        id: 1,
+        company: "GrantMatch",
+        title: "Junior Software Engineer",
+        period: "Jan 2020 - April 2020",
+        img: "./imgs/",
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
+    },
+    {
+        id: 2,
+        company: "UWAFT",
+        title: "Sensor Diagnostic Subteam Lead",
+        period: "Jan 2020 - April 2020",
+        img: "./imgs/",
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
+    },
+    {
+        id: 3,
+        company: "Flipp",
+        title: "Quality Assurance Intern",
+        period: "Jan 2020 - April 2020",
+        img: "./imgs/",
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
+    },
+    {
+        id: 4,
+        company: "416 Automation",
+        title: "Mechatronics Engineer",
+        period: "Jan 2020 - April 2020",
+        img: "./imgs/",
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
+    },
+    
 ]
 
 export default ExpData
