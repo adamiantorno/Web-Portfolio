9c3d33c50d874949ed74b0c9f6c998cafd5e741b
Adam I <adam.iantorno99@gmail.com>
2022-06-26 01:28:44 -0400

    exp func

--- web-portfolio/src/components/Experience/Experience.js
+++ web-portfolio/src/components/Experience/Experience.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from "react-bootstrap";
 
+import ExpData from '../../assets/expdata';
+
 export default function Experience() {
+
+  const [activeExpId, setActiveExpId] = useState(0);
+  const [experience, setExperience] = useState(ExpData);
+
   return (
     <section>
       <Container>
@@ -12,18 +18,43 @@ export default function Experience() {
             </Col>
           </Row>
           <Row>
-            <Col>
-              <Container>
-                <Button>
-
-                </Button>
-                <div></div>
-              </Container>
-              <Container>
-                <Container>
-                  
-                </Container>
-              </Container>
+            <Col xs={3}>
+              {experience.map((item) => {
+                return (
+                  <Button 
+                    key={item.id}
+                    onClick={() => setActiveExpId(item.id)}
+                  >
+                    {item.company}
+                  </Button>
+                );
+              })}
+            </Col>
+            <Col xs={9}>
+              {experience.map((item) => { 
+                return (
+                  <Container 
+                    key={item.id}
+                    className={activeExpId === item.id ? "active-exp" : "inactive-exp"}
+                    hidden={activeExpId !== item.id}
+                  >
+                    <h3>
+                      <span>{item.title}</span>
+                      <span> @{item.company}</span>
+                    </h3>
+                    <h4>
+                      <span>{item.period}</span>
+                    </h4>
+                    <ul>
+                      {item.desc.map((description) => {
+                        return (
+                          <li>{description}</li>
+                        );
+                      })}
+                    </ul>
+                  </Container>
+                )
+              })}
             </Col>
           </Row>
         </Container>
