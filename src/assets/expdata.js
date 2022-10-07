9570d49a05d4a10b2db57faf8a9e976b5e4996aa
Adam Iantorno <aiantorn@uwaterloo.ca>
2022-10-07 00:39:39 -0400

    pc changes

--- src/assets/expdata.js
+++ src/assets/expdata.js
@@ -1,6 +1,16 @@
 const ExpData = [
     {
         id: 0,
+        company: "Impossible Metals",
+        title: "Quality Assurance Intern",
+        period: "Jan 2020 - April 2020",
+        img: "./imgs/",
+        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin",
+                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
+    },
+    {
+        id: 1,
         company: "Electrans",
         title: "Mechatronics Engineer",
         period: "Jan 2020 - April 2020",
@@ -10,7 +20,7 @@ const ExpData = [
                 "Lorem ipsum dolor sit amet, consectetur adipiscin"]
     },
     {
-        id: 1,
+        id: 2,
         company: "GrantMatch",
         title: "Junior Software Engineer",
         period: "Jan 2020 - April 2020",
@@ -20,19 +30,9 @@ const ExpData = [
                 "Lorem ipsum dolor sit amet, consectetur adipiscin"]
     },
     {
-        id: 2,
+        id: 3,
         company: "UWAFT",
         title: "Sensor Diagnostic Subteam Lead",
-        period: "Jan 2020 - April 2020",
-        img: "./imgs/",
-        desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
-                "Lorem ipsum dolor sit amet, consectetur adipiscin",
-                "Lorem ipsum dolor sit amet, consectetur adipiscin"]
-    },
-    {
-        id: 3,
-        company: "Flipp",
-        title: "Quality Assurance Intern",
         period: "Jan 2020 - April 2020",
         img: "./imgs/",
         desc: ["Lorem ipsum dolor sit amet, consectetur adipiscin",
--- src/assets/strings.js
+++ src/assets/strings.js
@@ -1,14 +1,14 @@
 const APP_STRINGS = {
     adjectives: ['Engineering Student', 'Innovator', 'Self-Starter'],
     email: 'aiantorn@uwaterloo.ca',
-    about1: `I am a third year student studying mechancial engineering at the Univerisyt of Waterloo, Canada. 
-            Outside of school, I am a self-taught programmer with a passion for learning and expanding my skillset. 
-            My goal is to build sustainable and practical products that push the limits of technology and design.`,
+    about1: `Hi! My name is Adam and I'm a third year student studying mechancial engineering at the Univerisyt of Waterloo, Canada. 
+            Outside of school, I'm a self-taught programmer with a passion for learning and expanding my skillset. 
+            My goal is to build sustainable and applicable products that push the limits of technology and design.`,
     about2: `Currently, I am working as a Robotics Developer (co-op) at the start-up Impossible Metals.
             We are building underwater robotic vehciles that collect battery metals from the seabed without harming the environment.`,
     about3: `I am seeking a 4-month internship position for Spring 2023.`,
     skillset_design: ['Solidworkds', 'AutoCAD', 'Adobe Creative Suite'],
-    skillset_prog: ['Python', 'Javascript', 'ROS', 'C++'],
+    skillset_prog: ['Python', 'Javascript', 'ROS (2 Foxy)', 'C++'],
     skillset_other: ['Git', ],
 };
 
