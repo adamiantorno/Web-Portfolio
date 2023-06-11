e100a2839c2fc9bef31d3fff367933a114b6cd27
Adam Iantorno <aiantorn@uwaterloo.ca>
2023-06-10 23:10:51 -0400

    Create Mantle component for website overlay of header, footer, and sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Header from "./header/Header";
 import Iconbar from "./iconbar/Iconbar";
 import ProjectCard from "./project-card/ProjectCard";
 import Filter from "./project-card/Filter";
+import Mantle from "./mantle/Mantle";
 
 export {
     Footer,
     Header,
     Iconbar,
     ProjectCard,
-    Filter
+    Filter,
+    Mantle,
 };
---
+++ src/components/mantle/Mantle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './mantle.scss';
+
+export default function Mantle() {
+    return (
+        <div>
+            Mantle
+        </div>
+    )
+}
