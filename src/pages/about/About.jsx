3201e272fa49bb4376a9a00129836fb25e403ae8
Adam Iantorno <aiantorn@uwaterloo.ca>
2023-06-27 16:30:20 -0400

    Remove bubble from about me section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -28,112 +28,112 @@ export default function About() {
 
   return (
     <section className="section-page" id="about">
-      <Container fluid className="py-5">
-        <Container>
+      <Container>
+        <Row>
+          <Col>
+            <h2 className="section-title">About Me</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="about-text">
+            <p>
+              Hi! My name is Adam.
+              <span>&#128075;</span>
+            </p>
+            <p>
+              I'm a fourth-year student studying mechanical engineering at the
+              University of Waterloo, Canada. I am a self-taught programmer with
+              a passion for learning and expanding my skillset. My goal is to
+              build sustainable and applicable products that push the limits of
+              technology and design.
+            </p>
+            <p>
+              This past winter, I was working as a Robotics Developer at the
+              start-up {metalsLink}. We were building autonomous underwater
+              vehicles (AUVs) that harvest metal nodules from the seabed without
+              harming the environment.
+            </p>
+            <p>
+              Currently, I am a software engineer at the start-up working to
+              apply machine learning towards the legal industry.
+            </p>
+          </Col>
+          <Col>
+            <Image
+              className="about-pic-self"
+              rounded
+              src={aboutSelf}
+              alt="Adam Iantorno Waterloo Picture"
+            />
+          </Col>
+        </Row>
+        <hr className="splitter" />
+        <Row className="about-tech">
+          <span>
+            Here are a few technologies I have experience working with:
+          </span>
+          <Col>
+            <h4>Design</h4>
+            <ul>
+              {skillsetDesign.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </Col>
+          <Col>
+            <h4>Controls</h4>
+            <ul>
+              {skillsetProg.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </Col>
+          <Col>
+            <h4>Software</h4>
+            <ul>
+              {skillsetOther.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+        <hr className="splitter" />
+        <Row>
           <Row>
             <Col>
-              <h2 className="section-title-white">About Me</h2>
+              <h3 className="section-subtitle">Outside of Academia</h3>
             </Col>
           </Row>
-          <Row className="bubble justify-content-sm-center">
-            <Row className="about-info">
-              <Col className="about-text" md={8}>
-                <p>
-                  Hi! My name is Adam.{' '}
-                  <span style={{ fontSize: '1.5rem' }}>&#128075;</span>
-                </p>
-                <p>
-                  I'm a third year student studying mechanical engineering at
-                  the University of Waterloo, Canada. Outside of school, I'm a
-                  self-taught programmer with a passion for learning and
-                  expanding my skillset. My goal is to build sustainable and
-                  applicable products that push the limits of technology and
-                  design.
-                </p>
-                <p>
-                  Most recently, I was working as a Robotics Developer at the
-                  start-up {metalsLink}. We were building autonomous underwater
-                  vehicles (AUVs) that harvest metal nodules from the seabed
-                  without harming the environment.
-                </p>
-                <p>
-                  I am currently seeking a 4-month internship position for
-                  Summer 2023.
-                </p>
-              </Col>
-              <Col>
-                <Image
-                  className="about-pic"
-                  rounded
-                  src={aboutSelf}
-                  alt="Adam Iantorno Waterloo Portrait"
-                />
-              </Col>
-            </Row>
-            <Row className="about-tech">
-              <span>
-                Here are a few technologies I have experience working with:
-              </span>
-              <Col>
-                <h4>Design</h4>
-                <ul className="about-tech-list">
-                  {skillsetDesign.map((item) => (
-                    <li>{item}</li>
-                  ))}
-                </ul>
-              </Col>
-              <Col>
-                <h4>Programming</h4>
-                <ul>
-                  {skillsetProg.map((item) => (
-                    <li>{item}</li>
-                  ))}
-                </ul>
-              </Col>
-              <Col>
-                <h4>Other</h4>
-                <ul>
-                  {skillsetOther.map((item) => (
-                    <li>{item}</li>
-                  ))}
-                </ul>
-              </Col>
-            </Row>
-            <hr />
-            <Row className="about-fun">
-              <h3>Outside of Academia</h3>
-              <Col md={6} className="about-pic-div">
-                <Image
-                  className="about-adventure"
-                  src={aboutAll}
-                  alt="Adventure"
-                />
-              </Col>
-              <Col md={6} className="about-text py-3">
-                <p>
-                  {' '}
-                  Aside from engineering and projects, I enjoy having fun with
-                  my close friends and family, while also spending personal time
-                  to recharge.
-                </p>
-                <p>
-                  I am very invested in both weight-lifting and rock-climbing,
-                  and really enjoy going to the gym with my friends in Waterloo.
-                  I also enjoy cooking and reading. I am curerntly reading{' '}
-                  <span className="italics">The Poppy War by R. F. Kuang</span>{' '}
-                  , which is a historical fiction novel that draws its plot and
-                  politics from mid-20th-century China.
-                </p>
-                <p>
-                  I am constantly tinkering with this website and adding new
-                  features. I hope to add a blog section where I can post about
-                  my latest travels, books, and other interesting topics all in
-                  one place.
-                </p>
-              </Col>
-            </Row>
+          <Row>
+            <Col className="about-pic-div">
+              <Image
+                className="about-pic-fun"
+                rounded
+                src={aboutAll}
+                alt="Fun picture"
+              />
+            </Col>
+            <Col className="about-text">
+              <p>
+                Aside from school and work, I enjoy having fun with my close
+                friends and family, while also spending personal time to
+                recharge.
+              </p>
+              <p>
+                I am very invested in both weight-lighting and rock-climbing,
+                and really enjoy going to the gym with my friends in Waterloo. I
+                also enjoy cooking and reading. I am currently reading{' '}
+                <span className="italics">Outliers by Malcom Gladwell</span>,
+                which discussing niche and environmental factors that can lead
+                to high levels of success for individuals.
+              </p>
+              <p>
+                I am constantly tinkering with this website and adding new
+                features or updating certain designs.
+              </p>
+            </Col>
           </Row>
-        </Container>
+        </Row>
       </Container>
     </section>
   );
