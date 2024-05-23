520f489c2cad9e05c6f8c93b506c6902058fe85b
Adam Iantorno <aiantorn@uwaterloo.ca>
2024-05-22 20:16:45 -0400

    Update footer to two column design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,43 +1,71 @@
-import React from 'react';
+// Software Developed by Adam Iantorno for Portfolio Website 2024
+
+import React from "react";
+
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  GrGithub,
-  GrLinkedin,
-} from "react-icons/gr"
+import { GrGithub, GrLinkedin } from "react-icons/gr";
 
 import "./footer.scss";
 
+
+const socialLinks = [
+  {
+    name: 'Github',
+    icon: <GrGithub />,
+    url: 'https://www.github.com/adamiantorno',
+  },
+  {
+    name: 'LinkedIn',
+    icon: <GrLinkedin />,
+    url: 'https://www.linkedin.com/in/adamiantorno',
+  },
+];
+
 export default function Footer() {
-  return (
-    <Container fluid className="footer">
-      <Row>
-        <Col>
-          <h3 className="footer-title">Contact Me</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className='footer-socialicons'>
-            <a href="https://www.github.com/adamiantorno">
-              <GrGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/adamiantorno">
-              <GrLinkedin />
-            </a>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h4>Toronto, ON. Canada</h4>
-        </Col>
-        <Col>
-          <h4>Designed and Built by Adam Iantorno</h4>
-        </Col>
-        <Col>
-          <h4>aiantorn@uwaterloo.ca</h4>
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container fluid className="footer">
+            <Container fluid className="footer-container">            
+                <Row>
+                    <Col>
+                        <Row className="footer-subsection">
+                            <h3>ADAM IANTORNO</h3>
+                            <span>
+                                A versatile engineering student passionate about building innovating and sustainable products that push the limits of technology and design.
+                            </span>
+                        </Row>
+                    </Col>
+                    <Col md={2} />
+                    <Col>
+                        <Row className="footer-subsection">
+                            <Col>
+                                <h4>CONTACT</h4>
+                                <span>adam.iantorno99@gmail.com</span>
+                            </Col>
+                            <Col>
+                                <Container className="footer-social-links">
+                                    <ul>
+                                    {socialLinks.map(({ name, icon, url }) => (
+                                        <li key={name}>
+                                            <a href={url} aria-label={name}>
+                                                {icon}
+                                            </a>
+                                        </li>
+                                    ))}
+                                    </ul>
+                                </Container>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <hr />
+                <Row>
+                    <Col>
+                        <span>
+                            &copy; Copyright 2023. Designed and Built by Adam Iantorno
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
